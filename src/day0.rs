94db6269ff14cdad975bdd2c316f22611a884be7
flowalex <flowalex@vivaldi.net>
2024-12-31 12:11:24 -0600

    Shuttle CCH24 Submissions

---
+++ src/day0.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, web, Responder};
+use actix_web::http::StatusCode;
+use actix_web::web::Redirect;
+
+
+
+// Hello bird
+#[get("/")]
+async fn hello_world() -> &'static str {
+    "Hello, bird!"
+}
+
+// 302 Found redirection
+#[get("/-1/seek")]
+async fn seek() -> impl Responder {
+    Redirect::to("https://www.youtube.com/watch?v=9Gc4QTqslN4").using_status_code(StatusCode::FOUND)
+}
